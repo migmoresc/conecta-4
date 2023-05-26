c43ab6d56b038bf95c4d132837bd967aa6ced8df
Miguel Moreno <migmoresc@outlook.es>
2023-05-26 17:19:28 +0200

    tablero relleno y evento click en celda

--- scritps/script.js
+++ scritps/script.js
@@ -5,26 +5,51 @@ document.addEventListener("DOMContentLoaded", () => {
     document.getElementById("j2-ficha-color").addEventListener("click", cambiarColor);
     document.getElementById("j1-nombre").addEventListener("change", hayEscrito);
     document.getElementById("j2-nombre").addEventListener("change", hayEscrito);
+    // document.querySelector(".img-tablero").addEventListener("click", (e) => {
+    //     console.log(e.offsetX, e.offsetY)
+    // });
+    document.querySelector(".img-tablero").addEventListener("mouseover", (e) => {
+        e.target.style.cursor = "url('../assets/img/ficha-black.png') 36 36,auto";
+    });
+
+    document.querySelectorAll(".area-ficha").forEach((elem) => {
+        elem.addEventListener("mouseover", (e) => {
+            e.target.style.cursor = "url('../assets/img/ficha-black.png') 36 36,auto";
+        })
+    });
+
+    document.querySelectorAll(".area-ficha").forEach((elem) => {
+        elem.addEventListener("click", (e) => {
+            e.preventDefault(e.target);
+            console.log(e.target.id);
+        })
+    });
+
 });
 
 const colores = ["red", "green", "yellow", "black", "purple", "pink"];
 let j1_color, j2_color;
 let j1_nombre, j2_nombre;
+let turno;
 
 function jugar() {
     console.log("jugar")
     if (comprobarJugadores()) {
         document.querySelector(".jugadores").style.display = "none";
+        document.querySelector(".turno").style.display = "block";
         document.querySelector(".tablero").style.display = "block";
     }
 }
 
 function comprobarJugadores() {
-    console.log("comprobarjugadores")
+
     const j1_nombre = document.getElementById("j1-nombre");
     const j2_nombre = document.getElementById("j2-nombre");
     const j1_ficha_color = document.getElementById("j1-ficha-color");
     const j2_ficha_color = document.getElementById("j2-ficha-color");
+
+    window.j1_nombre = j1_nombre.value;
+    window.j2_nombre = j2_nombre.value;
 
     if (j1_nombre.value != "" && j2_nombre.value != "" && j1_color != undefined && j2_color != undefined && window.j1_nombre != window.j2_nombre) {
         return true;
